80f2c96de9139e36ea8323f4051564586849fe75
Tegospust <tegospust@gmail.com>
2023-01-30 23:46:30 +0100

    OK

---
+++ src/com/ounis/fo2tweaksrditor/CONST.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ounis.fo2tweaksrditor;
+
+/**
+ *
+ * @author AndroidDev
+ */
+public class CONST {
+    
+    public static final String FILE_FO2TWEAKS = "G:\\Gry\\Fallout 2\\mods\\fo2tweaks.ini"; 
+    
+}
---
+++ src/com/ounis/fo2tweaksrditor/Fo2Tweaks.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ounis.fo2tweaksrditor;
+
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author AndroidDev
+ */
+public class Fo2Tweaks {
+    Fo2Tweaks() {
+
+    }
+    
+    
+    public List<String> load() {
+    
+        return null;
+    }
+
+    public List<String> getSections() {
+        return null;
+    }
+    
+    public List<String> getItemsForSection(String aSectionName) {
+        return null;
+    }
+    
+}
